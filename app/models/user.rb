3c24f926543b422b99a395adc5ec07cd18270900
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-07 14:27:26 +0900

    バリデーションの修正と不要なコメントアウトの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,17 @@ class User < ApplicationRecord
   FURIGANA_REGEX = /\A(?=.*?[ァ-ヶー－])[ァ-ヶー－]+\z/i.freeze
   NAME_REGEX = /\A(?=.*?[ぁ-んァ-ヶ一-龥々])[ぁ-んァ-ヶ一-龥々]+\z/i.freeze
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: NAME_REGEX}
-  validates :last_name, presence: true, format: { with: NAME_REGEX}
-  validates :furigana_first_name, presence: true, format: { with: FURIGANA_REGEX}
-  validates :furigana_last_name, presence: true, format: { with: FURIGANA_REGEX}
-  validates :birth_date, presence: true
-  validates :password, format: { with: PASSWORD_REGEX }
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :password, format: { with: PASSWORD_REGEX }
+    with_options format: { with: NAME_REGEX} do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: FURIGANA_REGEX} do
+      validates :furigana_first_name, format: { with: FURIGANA_REGEX}
+      validates :furigana_last_name, format: { with: FURIGANA_REGEX}
+    end
+  end
 end
