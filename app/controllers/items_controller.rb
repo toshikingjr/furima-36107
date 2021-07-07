1367ffb0985e8e0cefffc6954b77ce28ac99b284
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-07 18:29:42 +0900

    写真投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_session
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,12 @@ class Item < ApplicationRecord
   belongs_to :status
 
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :explanation
+    validates :image
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     with_options numericality: { other_than: 1 } do
       validates :category_id
