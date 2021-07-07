1bfd2929ce8c9034bc136b7012dba1b1eee86923
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-07 17:48:59 +0900

    モデル・ビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.includes(:user)
   end
   
   def new
@@ -8,5 +8,17 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
