190165c6cc0408a24c49e9325b59248f9584a89c
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-08 11:27:58 +0900

    authenticate_user !メソッドの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_session, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
 
   end
@@ -21,11 +21,5 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 end
