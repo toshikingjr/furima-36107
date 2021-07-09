69fd783a7a4c0045c5fe3b79dc2e5e26bc277157
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-09 14:09:37 +0900

    PurchaseAddressクラスの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,7 +1,7 @@
 class PurchaseAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -12,7 +12,7 @@ class PurchaseAddress
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    purchase = Purchase.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
