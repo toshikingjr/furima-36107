216292fd26c3d850c068605f1a599b514ecd3c7e
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-10 11:55:39 +0900

    before_actionnの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index,  only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index,  only: [:edit, :update]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -42,7 +43,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
     if current_user.id != item.user_id
       redirect_to root_path
     end
