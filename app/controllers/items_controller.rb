f53a48d5cbbe6bfe3a6a57fd502f93388ca77ebe
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-11 10:41:20 +0900

    ビフォアアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != item.user_id
+    if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
