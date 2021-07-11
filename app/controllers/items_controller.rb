0d80bb43eb894a830e709ec58234514d0c6e9159
toshikingjr <86094280+toshikingjr@users.noreply.github.com>
2021-07-11 19:06:26 +0900

    編集画面への遷移に関するbefora_action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index,  only: [:edit, :update, :destroy]
+  before_action :already_purchase, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -52,5 +53,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def already_purchase
+    if @item.purchase != nil
+      redirect_to root_path
+    end
   end
 end
